ba442d6d66e2a91716ab28b2be0b69259bafd22a
fuzailmansuri <mfm9875@gmail.com>
2026-09-10 05:33:46 +0000

    compat: update audio and input ABI shims
    Change-Id: I810974e1ffebb2373fc50ae8c317230c0ced68e1

--- libaudioclient/AudioTrack.cpp
+++ libaudioclient/AudioTrack.cpp
@@ -55,7 +55,7 @@ class LegacyCallbackWrapper : public AudioTrack::IAudioTrackCallback {
 };
 #ifdef __LP64__
 #define orig \
-    _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tm20audio_output_flags_tRKNS_2wpINS0_19IAudioTrackCallbackEEEi15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi
+    _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tm20audio_output_flags_tRKNS_2wpINS0_19IAudioTrackCallbackEEEi15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfiRKNSt3__112basic_stringIcNSM_11char_traitsIcEENSM_9allocatorIcEEEE
 #else
 #define orig \
     _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tRKNS_2wpINS0_19IAudioTrackCallbackEEEi15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi
@@ -70,7 +70,8 @@ extern "C" void* orig(void* thisptr, audio_stream_type_t streamType, uint32_t sa
                       const audio_offload_info_t* offloadInfo,
                       const AttributionSourceState& attributionSource,
                       const audio_attributes_t* pAttributes, bool doNotReconnect,
-                      float maxRequiredSpeed, audio_port_handle_t selectedDeviceId);
+                      float maxRequiredSpeed, audio_port_handle_t selectedDeviceId,
+                      const std::string& codecProvenance);
 
 static wp<AudioTrack::IAudioTrackCallback> createCallback(legacy_callback_t callback, void* user) {
     if (callback) {
@@ -101,22 +102,22 @@ extern "C" void* newfnc(void* thisptr, audio_stream_type_t streamType, uint32_t 
     return orig(thisptr, streamType, sampleRate, format, channelMask, frameCount, flags,
                 createCallback(callback, user), notificationFrames, sessionId, transferType,
                 offloadInfo, attributionSource, pAttributes, doNotReconnect, maxRequiredSpeed,
-                selectedDeviceId);
+                selectedDeviceId, "");
 }
 
 #ifdef __LP64__
 extern "C" void*
-_ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tm20audio_output_flags_tRKNS_2wpINS0_19IAudioTrackCallbackEEEi15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfiRKNSt3__112basic_stringIcNSM_11char_traitsIcEENSM_9allocatorIcEEEE(
+_ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tm20audio_output_flags_tRKNS_2wpINS0_19IAudioTrackCallbackEEEi15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
         void* thisptr, audio_stream_type_t streamType, uint32_t sampleRate, audio_format_t format,
         audio_channel_mask_t channelMask, size_t frameCount, audio_output_flags_t flags,
         const wp<AudioTrack::IAudioTrackCallback>& callback, int32_t notificationFrames,
         audio_session_t sessionId, AudioTrack::transfer_type transferType,
         const audio_offload_info_t* offloadInfo, const AttributionSourceState& attributionSource,
         const audio_attributes_t* pAttributes, bool doNotReconnect, float maxRequiredSpeed,
-        audio_port_handle_t selectedDeviceId, const std::string& /* unknown */) {
+        audio_port_handle_t selectedDeviceId) {
     return orig(thisptr, streamType, sampleRate, format, channelMask, frameCount, flags, callback,
                 notificationFrames, sessionId, transferType, offloadInfo, attributionSource,
-                pAttributes, doNotReconnect, maxRequiredSpeed, selectedDeviceId);
+                pAttributes, doNotReconnect, maxRequiredSpeed, selectedDeviceId, "");
 }
 #endif
 
